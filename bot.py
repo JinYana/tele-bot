08d7219605fe9923a1a942f7693d579d95a6864c
JinYana <ngjinyang@gmail.com>
2022-07-03 23:04:25 +0800

    bug fixes

--- bot.py
+++ bot.py
@@ -423,12 +423,12 @@ def pregnantoptions(message):
 #                      chat_id=message)
 
 
-@server.route('/' + API_KEY, methods=['POST'])
-def getMessage():
-    json_string = request.get_data().decode('utf-8')
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return "goog", 200
+# @server.route('/' + API_KEY, methods=['POST'])
+# # def getMessage():
+# #     json_string = request.get_data().decode('utf-8')
+# #     update = telebot.types.Update.de_json(json_string)
+# #     bot.process_new_updates([update])
+# #     return "goog", 200
 
 
 @server.route("/")
